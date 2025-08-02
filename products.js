fde078edc040046cfbc2cc92ada3244a6392859d
Hadary50 <Hadary465@gmai.com>
2025-08-02 21:39:15 +0300

    ضيف products.js وضمن خطأ لو مش موجوfد

---
+++ products.js
@@ -0,0 +1,15 @@
+// products.js
+module.exports = [
+  {
+    id: "jet14",
+    name: "JET 14",
+    price: 1250,
+    description: "سكوتر كهربائي ممتاز",
+  },
+  {
+    id: "beach-buggy",
+    name: "Beach Buggy",
+    price: 3200,
+    description: "عربة شاطئ أنيقة",
+  },
+];
