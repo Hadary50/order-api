eec74a92509d361bcdc95dd96b20ab3c023a9b14
Hadary50 <Hadary465@gmai.com>
2025-08-02 21:48:46 +0300

    ضيف products.js وضمن خطأ لو مش موجود

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 const products = require("./products");
-const products = require("./products");
-
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
