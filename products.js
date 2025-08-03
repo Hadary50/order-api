2cc9859fbdedd62f2f4d47563f35b256252462fc
Hadary50 <Hadary465@gmai.com>
2025-08-03 16:25:18 +0300

    السعؤ

--- products.js
+++ products.js
@@ -3,8 +3,8 @@ module.exports = [
   {
     id: "jet14",
     name: "JET 14",
-    price: 1250,
-    description: "سكوتر بنزين  ممتاز",
+    price: 101900,
+    description: "Jet 14 DD سكوتر بنزين 200cc",
     images: [
       "https://aljoharips.com/upload/02-2020/article/01-JET-14_HeadLight%20(2).jpg",
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0R1dzesRyLMZ4JkOPNgkUfj-nPa8dc0GLww&s"
